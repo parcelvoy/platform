3ea45681be95fd6fe2230f824fe0b815851eeebe
Chris Anderson <hi@chrisanderson.io>
2023-02-09 21:35:40 -0600

    Tweak to migration

--- db/migrations/20230210024314_add_project_timezone.js
+++ db/migrations/20230210024314_add_project_timezone.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
     return knex.schema
         .table('projects', function(table) {
-            table.string('timezone', 50)
+            table.string('timezone', 50).after('locale')
         })
 }
 
