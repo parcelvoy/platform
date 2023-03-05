c6bacb123e5ad720e9937e26cadefc2f18e80586
Chris Anderson <hi@chrisanderson.io>
2023-03-04 18:52:26 -0600

    Next try

--- apps/ui/scripts/env.sh
+++ apps/ui/scripts/env.sh
@@ -5,8 +5,6 @@ ENV_JS="./config.js"
 rm -rf ${ENV_JS}
 touch ${ENV_JS}
 
-echo "window = {" >> ${ENV_JS}
 varname='API_BASE_URL'
 value=$(printf '%s\n' "${!varname}")
-echo "  $varname: \"$value\"," >> ${ENV_JS}
-echo "}" >> ${ENV_JS}
+echo "window.$varname: \"$value\"," >> ${ENV_JS}
