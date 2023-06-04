89633834e82f89cb9604f07532e11fc43552e110
Chris Anderson <hi@chrisanderson.io>
2023-06-03 20:34:24 -0500

    Adds last stage fallback for UI API

--- apps/ui/src/config/env.ts
+++ apps/ui/src/config/env.ts
@@ -4,6 +4,6 @@ declare global {
 
 export const env = {
     api: {
-        baseURL: window.API_BASE_URL ?? process.env.REACT_APP_API_BASE_URL,
+        baseURL: window.API_BASE_URL ?? process.env.REACT_APP_API_BASE_URL ?? '/api',
     },
 }
