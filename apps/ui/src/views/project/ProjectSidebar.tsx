253378987df629495dd134c078279ee1b898deb5
Chris Anderson <hi@chrisanderson.io>
2023-06-17 15:47:30 -0500

    Fixes where view all takes you

--- apps/ui/src/views/project/ProjectSidebar.tsx
+++ apps/ui/src/views/project/ProjectSidebar.tsx
@@ -59,7 +59,7 @@ export default function ProjectSidebar({ children, links }: PropsWithChildren<Si
                     value={project}
                     onChange={project => {
                         if (project.id === 0) {
-                            navigate('/')
+                            navigate('/settings/projects')
                         } else {
                             navigate(`/projects/${project.id}`)
                         }
