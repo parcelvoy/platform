87b052e1172a8075936beda44d6f635503d81408
Chris Anderson <hi@chrisanderson.io>
2023-06-21 21:04:03 -0500

    Adjusts what gets set as default env values (#195)

--- apps/ui/src/config/env.ts
+++ apps/ui/src/config/env.ts
@@ -4,6 +4,7 @@ declare global {
 
 export const env = {
     api: {
-        baseURL: window.API_BASE_URL ?? process.env.REACT_APP_API_BASE_URL ?? '/api',
+        baseURL: window.API_BASE_URL
+            || (process.env.REACT_APP_API_BASE_URL ?? '/api'),
     },
 }
