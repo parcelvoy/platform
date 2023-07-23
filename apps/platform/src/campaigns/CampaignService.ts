4596907458d0b53779059153acb6c1f2f50ddac1
Chris Anderson <hi@chrisanderson.io>
2023-07-23 09:18:38 -0500

    Prevents campaign from regenerating list

--- apps/platform/src/campaigns/CampaignService.ts
+++ apps/platform/src/campaigns/CampaignService.ts
@@ -46,7 +46,10 @@ export const allCampaigns = async (projectId: number): Promise<Campaign[]> => {
 }
 
 export const getCampaign = async (id: number, projectId: number): Promise<Campaign | undefined> => {
-    const campaign = await Campaign.find(id, qb => qb.where('project_id', projectId))
+    const campaign = await Campaign.find(id,
+        qb => qb.where('project_id', projectId)
+            .whereNull('deleted_at'),
+    )
 
     if (campaign) {
         campaign.templates = await allTemplates(projectId, campaign.id)
