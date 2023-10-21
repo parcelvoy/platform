8adcea98a11133889ced978f96813ea13a4afeac
Chris Anderson <hi@chrisanderson.io>
2023-10-21 08:57:43 -0500

    Minor UI tweaks to org creation

--- apps/ui/src/views/organization/Admins.tsx
+++ apps/ui/src/views/organization/Admins.tsx
@@ -1,13 +1,18 @@
-import { useCallback } from 'react'
+import { useCallback, useContext } from 'react'
 import PageContent from '../../ui/PageContent'
 import { SearchTable, useSearchTableQueryState } from '../../ui/SearchTable'
 import api from '../../api'
+import { Alert } from '../../ui'
+import { OrganizationContext } from '../../contexts'
 
 export default function Admins() {
     const state = useSearchTableQueryState(useCallback(async params => await api.admins.search(params), []))
+    const [organization] = useContext(OrganizationContext)
+    const url = `${window.location.protocol}//${organization.username}.${window.location.hostname}:${window.location.port}`
     return (
         <>
             <PageContent title="Admins">
+                <Alert title="Invite Link">To be joined to your organization, send users an invite from a project or have them register using the following link: <br/><strong>{url}</strong></Alert>
                 <SearchTable
                     {...state}
                     columns={[
