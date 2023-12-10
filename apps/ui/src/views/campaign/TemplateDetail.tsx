db619a6d55cda4644825fb01f6e5588af7c9b31a
Chris Anderson <hi@chrisanderson.io>
2023-12-10 10:13:13 -0600

    Fixes linter issue

--- apps/ui/src/views/campaign/TemplateDetail.tsx
+++ apps/ui/src/views/campaign/TemplateDetail.tsx
@@ -163,7 +163,6 @@ export default function TemplateDetail({ template }: TemplateDetailProps) {
                     }
                 </Column>
 
-                <Column>
                 <Column fullscreen={true}>
                     <Heading title="Design" size="h4" actions={
                         type === 'email' && campaign.state !== 'finished' && <LinkButton size="small" variant="secondary" to={`../editor?locale=${template.locale}`}>Edit Design</LinkButton>
