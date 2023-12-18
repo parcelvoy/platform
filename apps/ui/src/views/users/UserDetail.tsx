2e10489453a6f63f051822de9e62b17fc2baeea2
Chris Anderson <hi@chrisanderson.io>
2023-12-18 17:11:11 -0600

    Allows deleting a user from admin UI

--- apps/ui/src/views/users/UserDetail.tsx
+++ apps/ui/src/views/users/UserDetail.tsx
@@ -1,19 +1,36 @@
 import { useContext } from 'react'
-import { UserContext } from '../../contexts'
+import { ProjectContext, UserContext } from '../../contexts'
 import PageContent from '../../ui/PageContent'
 import { NavigationTabs } from '../../ui/Tabs'
 import { Outlet } from 'react-router'
 import { InfoTable } from '../../ui/InfoTable'
+import { Button } from '../../ui'
+import { TrashIcon } from '../../ui/icons'
+import api from '../../api'
+import { useNavigate } from 'react-router-dom'
 
 export default function UserDetail() {
 
-    const [{ external_id, email, phone, timezone, full_name }] = useContext(UserContext)
+    const navigate = useNavigate()
+    const [project] = useContext(ProjectContext)
+    const [{ id, external_id, email, phone, timezone, full_name }] = useContext(UserContext)
+
+    const deleteUser = async () => {
+        if (confirm('Are you sure you want to delete this user? All existing data will be removed.\n\nNote: If new data is sent for this user, they will be re-created with whatever data is sent.')) {
+            await api.users.delete(project.id, id)
+            navigate(`/projects/${project.id}/users`)
+        }
+    }
 
     return (
         <PageContent
             title={full_name ?? (email ?? 'No email')}
             desc={
                 <InfoTable rows={{ ID: external_id, email, phone, timezone }} direction="horizontal" />
+            }
+            actions={
+                <Button icon={<TrashIcon />}
+                    onClick={deleteUser}>Delete User</Button>
             }
         >
             <NavigationTabs tabs={[
