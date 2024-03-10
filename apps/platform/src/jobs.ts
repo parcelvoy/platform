aaf80e79368ba0ad3d5eeead77b7d5f2714e5c64
chills <christopherhills292@gmail.com>
2024-03-10 06:20:47 -0500

    export jobs

---
+++ apps/platform/src/jobs.ts
@@ -0,0 +1,3 @@
+export { default as EventPostJob } from './client/EventPostJob'
+export { default as UserAliasJob } from './users/UserAliasJob'
+export { default as UserPatchJob } from './users/UserPatchJob'
