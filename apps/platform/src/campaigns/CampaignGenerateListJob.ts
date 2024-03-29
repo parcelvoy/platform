fb19621d8b11dc4424afdad6b47ed0b576bac8bd
Chris Anderson <hi@chrisanderson.io>
2024-03-28 23:40:25 -0500

    Fixes naming of logging

--- apps/platform/src/campaigns/CampaignGenerateListJob.ts
+++ apps/platform/src/campaigns/CampaignGenerateListJob.ts
@@ -15,7 +15,7 @@ export default class CampaignGenerateListJob extends Job {
     static async handler({ id, project_id }: CampaignJobParams) {
         const key = `campaign_generate_${id}`
 
-        logger.info(`${this.$name}:loading`, { id })
+        logger.info('campaign:generate:loading', { id })
         const campaign = await getCampaign(id, project_id) as SentCampaign
         if (!campaign) return
         if (campaign.state === 'aborted' || campaign.state === 'draft') return
@@ -23,16 +23,16 @@ export default class CampaignGenerateListJob extends Job {
         // Increase lock duration based on estimated send size
         const estimatedSize = await estimatedSendSize(campaign)
         const lockTime = Math.max(estimatedSize / 1000, 900)
-        logger.info(`${this.$name}:estimated_size`, { id, estimatedSize, lockTime })
+        logger.info('campaign:generate:estimated_size', { id, estimatedSize, lockTime })
 
         const acquired = await acquireLock({ key, timeout: lockTime })
-        logger.info(`${this.$name}:lock`, { id, acquired })
+        logger.info('campaign:generate:lock', { id, acquired })
         if (!acquired) return
 
-        logger.info(`${this.$name}:querying`, { id })
+        logger.info('campaign:generate:querying', { id })
         await generateSendList(campaign)
 
-        logger.info(`${this.$name}:sending`, { id })
+        logger.info('campaign:generate:sending', { id })
         await CampaignEnqueueSendsJob.from({
             id: campaign.id,
             project_id: campaign.project_id,
