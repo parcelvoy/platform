1eb7ab5dbfac343614054cb102e0e8d35d6b5d32
Chris Anderson <hi@chrisanderson.io>
2024-04-01 09:50:12 -0500

    Properly duplicates journey campaigns

--- apps/platform/src/campaigns/CampaignService.ts
+++ apps/platform/src/campaigns/CampaignService.ts
@@ -350,7 +350,7 @@ export const abortCampaign = async (campaign: Campaign) => {
 export const duplicateCampaign = async (campaign: Campaign) => {
     const params: Partial<Campaign> = pick(campaign, ['project_id', 'list_ids', 'exclusion_list_ids', 'provider_id', 'subscription_id', 'channel', 'name', 'type'])
     params.name = `Copy of ${params.name}`
-    params.state = 'draft'
+    params.state = campaign.type === 'blast' ? 'draft' : 'running'
     const cloneId = await Campaign.insert(params)
     for (const template of campaign.templates) {
         await duplicateTemplate(template, cloneId)
