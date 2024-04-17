2c597a01f7f3d4e2656fa8baa8baa04110aee122
Chris Anderson <hi@chrisanderson.io>
2024-04-16 20:13:58 -0500

    Allows for rebuilding path suggestions in UI

--- apps/ui/src/views/settings/ProjectSettings.tsx
+++ apps/ui/src/views/settings/ProjectSettings.tsx
@@ -4,10 +4,17 @@ import Heading from '../../ui/Heading'
 import { toast } from 'react-hot-toast/headless'
 import ProjectForm from '../project/ProjectForm'
 import { useTranslation } from 'react-i18next'
+import { Button } from '../../ui'
+import api from '../../api'
 
 export default function ProjectSettings() {
     const { t } = useTranslation()
     const [project, setProject] = useContext(ProjectContext)
+
+    const handleRebuildAttributeSchema = async () => {
+        await api.projects.rebuildPathSuggestions(project.id)
+        toast.success(t('rebuild_path_suggestions_success'))
+    }
 
     return (
         <>
@@ -17,6 +24,14 @@ export default function ProjectSettings() {
                 setProject(project)
                 toast.success(t('project_settings_saved'))
             }} />
+
+            <br />
+            <Heading size="h3" title={t('advanced')} />
+            <label>
+                <span>{t('rebuild_path_suggestions')}</span>
+                <span className="label-subtitle">{t('rebuild_path_suggestions_desc')}</span>
+                <Button variant="secondary" style={{ marginTop: '10px' }} onClick={async () => await handleRebuildAttributeSchema()}>{t('rebuild')}</Button>
+            </label>
         </>
     )
 }
