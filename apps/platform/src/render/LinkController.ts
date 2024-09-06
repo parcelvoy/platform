5dc69fe4522560642b325e9814be30692b77719a
Chris Anderson <hi@chrisanderson.io>
2024-09-05 20:20:31 -0500

    Improve universal link referencing and performance

--- apps/platform/src/render/LinkController.ts
+++ apps/platform/src/render/LinkController.ts
@@ -2,6 +2,7 @@ import Router from '@koa/router'
 import App from '../app'
 import { encodedLinkToParts, trackMessageEvent } from './LinkService'
 import Organization from '../organizations/Organization'
+import { cacheGet, cacheSet } from '../config/redis'
 
 const router = new Router<{
     app: App
@@ -30,14 +31,24 @@ router.get('/o', async ctx => {
 })
 
 router.get('/.well-known/:file', async ctx => {
-    const url = ctx.state.organization?.tracking_deeplink_mirror_url
+    const organization = ctx.state.organization
+    const url = organization?.tracking_deeplink_mirror_url
     const file = ctx.params.file
     if (!url) {
         ctx.status = 404
         return
     }
-    const response = await fetch(`${url}/.well-known/${file}`)
-    ctx.body = await response.json()
+
+    const key = `well-known:${organization.id}:${file}`
+    const value = await cacheGet<any>(App.main.redis, key)
+    if (value) {
+        ctx.body = value
+    } else {
+        const response = await fetch(`${url}/.well-known/${file}`)
+        const value = await response.json()
+        await cacheSet(App.main.redis, key, value, 60 * 60 * 5)
+        ctx.body = value
+    }
 })
 
 export default router
