6e59f20899087fc016a9cab7e9b3adce25cb1373
Chris Anderson <hi@chrisanderson.io>
2024-10-11 22:59:23 -0500

    Tweak proxy for universal link registration

--- apps/platform/src/organizations/OrganizationMiddleware.ts
+++ apps/platform/src/organizations/OrganizationMiddleware.ts
@@ -4,13 +4,13 @@ import App from '../app'
 
 export const organizationMiddleware = async (ctx: Context, next: () => void) => {
     const organizationId = ctx.cookies.get('organization', { signed: true })
-    if (organizationId) {
+    if (!App.main.env.config.multiOrg) {
+        ctx.state.organization = await getDefaultOrganization()
+    } else if (organizationId) {
         ctx.state.organization = await getOrganization(parseInt(organizationId))
     } else if (ctx.subdomains && ctx.subdomains[0]) {
         const subdomain = ctx.subdomains[0]
         ctx.state.organization = await getOrganizationByUsername(subdomain)
-    } else if (App.main.env.config.multiOrg) {
-        ctx.state.organization = await getDefaultOrganization()
     }
     return next()
 }
