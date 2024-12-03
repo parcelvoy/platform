04df9139a2f9a50f866289fdb59f57d3b9ea613a
Chris Anderson <hi@chrisanderson.io>
2024-12-02 20:39:24 -0600

    Fixes build error

--- apps/platform/src/providers/push/LoggerPushProvider.ts
+++ apps/platform/src/providers/push/LoggerPushProvider.ts
@@ -27,6 +27,7 @@ export default class LoggerPushProvider extends PushProvider {
             push,
             success: true,
             response: '',
+            invalidTokens: [],
         }
     }
 
