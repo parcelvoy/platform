e12d44a6f5ea8fbc76fea4b634351bac5cd49bcf
Chris Anderson <hi@chrisanderson.io>
2024-12-03 12:57:43 -0600

    Disables pretty printing in production

--- apps/platform/src/config/logger.ts
+++ apps/platform/src/config/logger.ts
@@ -2,10 +2,9 @@ import pino from 'pino'
 import pretty from 'pino-pretty'
 import ErrorHandler, { ErrorConfig } from '../error/ErrorHandler'
 
-// TODO: Check ENV and disable prettier for production
 export const logger = pino({
     level: process.env.LOG_LEVEL || 'warn',
-}, pretty({ colorize: true }))
+}, process.env.LOG_PRETTY_PRINT ? pretty({ colorize: true }) : undefined)
 
 export default async (config: ErrorConfig) => {
     return new ErrorHandler(config)
