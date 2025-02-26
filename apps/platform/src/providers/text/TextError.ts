035ff0eab538a4bac95b9b597d53ebe155775a16
Chris Anderson <hi@chrisanderson.io>
2025-02-26 10:43:00 -0600

    Improves error handling for SMS sending

--- apps/platform/src/providers/text/TextError.ts
+++ apps/platform/src/providers/text/TextError.ts
@@ -2,10 +2,10 @@ import { ContextError } from '../../error/ErrorHandler'
 
 export default class TextError extends ContextError {
     phone: string
-    constructor(type: string, phone: string, message: string) {
+    constructor(type: string, phone: string, message: string, context: any = {}) {
         super(`Text Error: ${message}`)
         this.phone = phone
-        this.context = { phone, type }
+        this.context = { phone, type, ...context }
         Error.captureStackTrace(this, TextError)
     }
 }
